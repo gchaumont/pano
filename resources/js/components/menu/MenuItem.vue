251299a6fea8157c9e99e2bd459181ae08f5e84d
Guy <gchaumont@me.com>
2022-08-23 17:52:11 +0200

    External link Icon

--- resources/js/components/menu/MenuItem.vue
+++ resources/js/components/menu/MenuItem.vue
@@ -3,7 +3,7 @@
         <component :is="config.icon" v-if="config.icon" class="h-5 w-5" />
         <span class="ml-3">{{config.name}}</span>
     </router-link>
-    <a  v-if="config.path && isAbsolute" :href="config.path">
+    <a  v-if="config.path && isAbsolute" :href="config.path" class="flex items-center p-2 text-base font-normal text-slate-700 my-0.5 hover:text-slate-900 rounded-lg dark:text-slate-400 hover:bg-slate-300  dark:hover:bg-slate-800 dark:hover:text-slate-300">
         <component :is="config.icon" v-if="config.icon" class="h-5 w-5" />
         <span class="ml-3">{{config.name}}</span>
     </a>
